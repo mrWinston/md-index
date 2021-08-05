f37e39c78fda045aa2d115f301c065b2520bbd90
mrWinston <2656597+mrWinston@users.noreply.github.com>
2021-08-05 15:30:49 +0200

    Readme create, simple function for finding md files

---
+++ README.md
@@ -0,0 +1,3 @@
+# MD-Index
+
+Simple golang utility to create index markdown files in folders and subfolders full of markdown files.
--- main.go
+++ main.go
@@ -1,6 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+)
+
+func findMarkdownFilesInDirectory(dir string) ([]string, error) {
+	markdownFiles := []string{}
+	dircontent, err := ioutil.ReadDir(dir)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, file := range dircontent {
+		fullPath := filepath.Join(dir, file.Name())
+		if filepath.Ext(fullPath) == ".md" {
+			markdownFiles = append(markdownFiles, fullPath)
+		}
+	}
+	return markdownFiles, nil
+}
 
 func main() {
 	fmt.Println("hello World")
